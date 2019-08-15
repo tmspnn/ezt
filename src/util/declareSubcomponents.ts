6fa854390b427a468ef6b0a575740e09250b8b31
Thomas Peng <tmspnn@gmail.com>
2019-08-15 15:46:06 +0800

    fix(duplicate eztid generation)

--- src/util/declareSubcomponents.ts
+++ src/util/declareSubcomponents.ts
@@ -5,13 +5,13 @@ export default function declareSubcomponents(
   data: { [k: string]: any },
   subcomponents: { [k: string]: SubcomponentDeclaration } | Array<SubcomponentDeclaration>
 ) {
+  if (data.$) return;
   if (Array.isArray(subcomponents)) subcomponents = { ...subcomponents };
   data._$ = subcomponents;
   data.$ = {};
   for (let k in subcomponents) {
     if (subcomponents.hasOwnProperty(k)) {
       const sc = (subcomponents as { [k: string]: SubcomponentDeclaration })[k];
-      if (sc.data.eztid) return;
       sc.data.eztid = getComponentId();
       data.$[k] = sc.fn(sc.data);
     }
