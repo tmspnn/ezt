f5361a75066e9872e7cb1aa7fe6f5e5e5eecb289
tmspnn <tmspnn@gmail.com>
2020-06-03 14:16:06 +0800

    feat: add an API "respondTo".

--- src/ezt.ts
+++ src/ezt.ts
@@ -1,6 +1,25 @@
+import { Subscription, UnaryFunction } from "rxjs";
+import { filterReaction } from "./interactions";
 import createComponent from "./component/createComponent";
-export { default as createComponent } from "./component/createComponent";
+
 export { default as Controller } from "./Controller";
-export { dispatchAction as dispatch, filterReaction as respondTo } from "./interactions";
+export { default as createComponent } from "./component/createComponent";
+export { dispatchAction as dispatch } from "./interactions";
 
 export default createComponent;
+
+export function respondTo(
+  reaction: string,
+  pipes: () => void | UnaryFunction<any, any> | UnaryFunction<any, any>[],
+  handler?: () => void
+): Subscription {
+  const observable = filterReaction(reaction);
+  const subscribable = !handler
+    ? observable
+    : Array.isArray(pipes)
+    ? (observable as any).pipe(...(pipes as UnaryFunction<any, any>[]))
+    : observable.pipe(pipes as UnaryFunction<any, any>);
+  return subscribable.subscribe(handler || pipes, (e: any) => {
+    console.error(`Error in ${reaction}: ${e}`);
+  });
+}
